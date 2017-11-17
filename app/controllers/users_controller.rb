94ab88e204a2070fab6431dd26b5faefc756fdd5
ndalcin <dalcin.nicole@gmail.com>
2017-11-17 14:26:56 -0500

    old code, restarting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,6 +41,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name)
+      params.require(:user).permit(:name, :post_ids => [], :post_attributes => [:name, :content])
     end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,5 +2,5 @@ class Tag < ActiveRecord::Base
   has_many :post_tags
   has_many :posts, :through => :post_tags
 
-  validates_uniqueness_of :name
+  validates :name, presence: true, uniqueness: true
 end
